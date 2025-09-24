80071d021603255a329c63fc5d069abf3c4b55b3
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2025-09-24 13:00:35 -0400

    lec07

---
+++ lec07/exceptions/Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+
+    public static void main(String[] args) {
+        Student s = new Student("Cinna", -1.0, "park");
+
+    }
+}
---
+++ lec07/objectComparison/Student.java
@@ -0,0 +1,49 @@
+class Student implements Comparable {
+  private String name;
+  private double gpa;
+  private String dorm;
+
+  public Student(String name, double gpa, String dorm) {
+    this.name = name;
+    this.gpa = gpa;
+    this.dorm = dorm;
+  }
+
+  public Student() {}
+
+  public String getName() {
+    return name;
+  }
+
+  public double getGpa() {
+    return gpa;
+  }
+
+  public String getDorm() {
+    return dorm;
+  }
+
+  public String toString() {
+    return name + " " + gpa + " " + dorm;
+  }
+
+  public boolean equals(Student other) {
+    return this.name.equals(other.getName()) && this.gpa == other.getGpa() 
+            && this.dorm.equals(other.getDorm());
+  }
+
+  public int compareTo(Object o) {
+        Student other = (Student) o;
+
+        //return 0 -> equals
+        if (this.gpa == other.getGpa()) {
+            return 0;
+        } else if (this.gpa < other.getGpa()) {
+            return -1;
+        } else {
+            return 1;
+        }
+  }
+
+}
+
